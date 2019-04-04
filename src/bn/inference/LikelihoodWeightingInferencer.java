05cb952f9ebaa6c49c26d46e93fb822f1df7a90c
Brandon Toops <btoops@u.rochester.edu>
2019-04-03 21:43:50 -0400

    WeightedSample

--- src/bn/inference/LikelihoodWeightingInferencer.java
+++ src/bn/inference/LikelihoodWeightingInferencer.java
@@ -1,5 +1,44 @@
 package bn.inference;
 
+import bn.core.Assignment;
+import bn.core.BayesianNetwork;
+import bn.core.RandomVariable;
+
 public class LikelihoodWeightingInferencer {
 
+
+
+
+
+
+
+
+	public class WeightedAssignment{
+
+		double weight;
+		Assignment event;
+
+		public WeightedAssignment(Assignment a, double w) {
+			weight = w;
+			event = a;
+		}
+
+	}
+
+
+
+
+	public WeightedAssignment weightedSample(BayesianNetwork bn, Assignment e) {
+		double weight = 1;
+		Assignment x = e.copy();
+		for(RandomVariable v : bn.getVariablesSortedTopologically()) {
+			if(e.containsKey(v)) {
+				
+			}
+		}
+		
+		
+		
+	}
 }
+
