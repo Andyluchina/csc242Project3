f115a11e0f9366535dac98041c54b4d01b00fe9e
Brandon Toops <btoops@u.rochester.edu>
2019-04-03 22:14:00 -0400

    Sudoku

--- src/bn/inference/LikelihoodWeightingInferencer.java
+++ src/bn/inference/LikelihoodWeightingInferencer.java
@@ -1,19 +1,28 @@
 package bn.inference;
 
-import java.util.ArrayList;
-
 import bn.base.Domain;
 import bn.core.Assignment;
 import bn.core.BayesianNetwork;
+import bn.core.Distribution;
 import bn.core.RandomVariable;
-import bn.core.Value;
 
 public class LikelihoodWeightingInferencer {
-
-
-
-
-
+	
+	public Distribution LikelihoodWeighting(RandomVariable X, Assignment e, BayesianNetwork bn, int N) {
+		Distribution dist = new bn.base.Distribution(X);
+		
+		for(int j = 0; j < X.getDomain().size(); j++) {
+			Domain domain = (Domain) X.getDomain();
+			dist.put(domain.getElements().get(j), 0.0);
+		}
+		System.out.println(dist);
+		for(int i = 0; i<N; i++) {
+			WeightedAssignment sample = this.weightedSample(bn, e);
+			System.out.println(sample);
+		}
+		
+		return null;
+	}
 
 
 
@@ -34,57 +43,22 @@ public class LikelihoodWeightingInferencer {
 
 	}
 
-	public Assignment priorSample(BayesianNetwork bn) {
-
-		Assignment e = new bn.base.Assignment();
-		ArrayList<RandomVariable> topSort = (ArrayList<RandomVariable>) bn.getVariablesSortedTopologically();
-		for(int i = 0; i < topSort.size(); i++) {
-			RandomVariable xi = topSort.get(i);
-			ArrayList<Double> dist= new ArrayList();
-			for(int j = 0; j < xi.getDomain().size(); j++) {
-				Domain domain = (Domain) xi.getDomain();
-				e.put(xi, domain.getElements().get(j));
-				//System.out.println(e);
-				dist.add(bn.getProbability(xi, e));
-				e.remove(xi);
-			}
-			double chance = Math.random();
-			int index = this.getIndexWithChance(chance, dist);
-			Domain domain = (Domain) xi.getDomain();
-			e.put(xi, domain.getElements().get(index));
-			//System.out.println(dist);
-		}
-//		System.out.println(e);
-		
-		return e;
-	}
-	
-	public int getIndexWithChance(double chance, ArrayList<Double> dist) {
-		for(int i=0; i<dist.size(); i++) {
-			chance-=dist.get(i);
-			if(chance<=0) {
-				return i;
-			}
-		}
-		return 0;
-	}
 
 
 	public WeightedAssignment weightedSample(BayesianNetwork bn, Assignment e) {
 		double weight = 1;
+		RejectionSamplingInferencer rsi = new RejectionSamplingInferencer();
 		Assignment x = e.copy();
 		for(RandomVariable v : bn.getVariablesSortedTopologically()) {
 			if(e.containsKey(v)) {
 				x.put(v, e.get(v));
 				weight *= bn.getProbability(v, x);
 			}else {
-				x.put(v,  priorSample(bn));
+				x.put(v, rsi.priorSample(bn).get(v));
 			}
 		}
 		WeightedAssignment ans = new WeightedAssignment(x, weight);
-		
-		System.out.println("ans");
-		
+		System.out.println(ans);
 		return ans;
 		
 		
